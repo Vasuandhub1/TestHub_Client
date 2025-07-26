6b9e93bf05421b32e808d4546e28b79a23c16c8e
vasu singh <vasusingh9691@gmail.com>
2025-07-26 12:01:14 +0530

    resolved bugs

--- utils/url.ts
+++ utils/url.ts
@@ -1,7 +1,7 @@
-// export const Base_Url="https://testhub-server-0kx4.onrender.com"
-// export const Student_Base_URL = "https://testhub-server-0kx4.onrender.com/student-test-hub"
+export const Base_Url="https://testhub-server-0kx4.onrender.com"
+export const Student_Base_URL = "https://testhub-server-0kx4.onrender.com/student-test-hub"
 export const  emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 
-export const Base_Url="http://localhost:3000"
-export const Student_Base_URL = "http://localhost:3000/student-test-hub"
+// export const Base_Url="http://localhost:3000"
+// export const Student_Base_URL = "http://localhost:3000/student-test-hub"
